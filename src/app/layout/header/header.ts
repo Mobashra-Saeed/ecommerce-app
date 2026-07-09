65d1fe51c6601d7cd46848f2253edf6acd383b18
Mobashra Saeed <mobashrasaeed713@gmail.com>
2026-07-09 12:00:42 +0500

    added logo

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -7,11 +7,14 @@ import { HeaderActions } from '../header-actions/header-actions';
   imports: [MatToolbarModule, HeaderActions],
   template: ` 
   <mat-toolbar class="w-full elevated py-2">
-    <div class="max-w-[1200px] mx-auto w-full flex items-center justify-between px-4 sm:px-6">
-      <span class="text-lg sm:text-xl font-bold">Modern Store</span>
-   
-    <app-header-actions/>
-     </div>
+    <<div class="max-w-[1200px] mx-auto w-full flex items-center justify-between px-4 sm:px-6">
+  
+  <a routerLink="/" class="flex items-center gap-2 sm:gap-3 cursor-pointer no-underline text-inherit hover:opacity-80 transition-opacity">
+    <img src="favicon.png" alt="Modern Store Logo" class="w-8 h-8 sm:w-10 sm:h-10 object-contain" />
+    <span class="text-lg sm:text-xl font-bold">Modern Store</span>
+  </a>
+  <app-header-actions/>
+    </div>
   </mat-toolbar>
   `,
   styles: ``,
